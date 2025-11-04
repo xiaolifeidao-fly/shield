52f102ace588a5c9712f2c7d4b7caa91e310b711
jiiuijiu <jiujjiu@163.com>
2025-11-04 10:25:44 +0800

    1

--- client/mac/dev.sh
+++ client/mac/dev.sh
@@ -1,30 +1,4 @@
 
-# 检查 resource/mac 目录下是否存在 @img 和 sharp 文件夹
-if [ -d "resource/mac/@img" ] && [ -d "resource/mac/sharp" ]; then
-    echo "Found cached @img and sharp folders, copying to node_modules..."
-    # 删除现有的文件夹
-    rm -rf node_modules/@img
-    rm -rf node_modules/sharp
-    
-    # 从缓存复制文件夹到 node_modules
-    cp -r resource/mac/@img node_modules/
-    cp -r resource/mac/sharp node_modules/
-else
-    echo "No cached folders found, installing sharp..."
-    # 删除现有的文件夹
-    rm -rf node_modules/@img
-    rm -rf node_modules/sharp
-    
-    # 安装 sharp
-    npm install sharp
-    
-    # 确保 resource/mac 目录存在
-    mkdir -p resource/mac
-    
-    # 复制到 resource/mac 作为缓存
-    cp -r node_modules/@img resource/mac/
-    cp -r node_modules/sharp resource/mac/
-fi
 cp -rf static/html ../resource/ 
 webpack --config webpack.config.js --mode development
 electron .
--- client/win/dev.bat
+++ client/win/dev.bat
@@ -1,31 +1,5 @@
 @echo off
 
-REM 检查 resource/win 目录下是否存在 @img 和 sharp 文件夹
-if exist "resource\win\@img" if exist "resource\win\sharp" (
-    echo Found cached @img and sharp folders, copying to node_modules...
-    REM 删除现有的文件夹
-    if exist "node_modules\@img" rmdir /s /q "node_modules\@img"
-    if exist "node_modules\sharp" rmdir /s /q "node_modules\sharp"
-    
-    REM 从缓存复制文件夹到 node_modules
-    xcopy /E /I /Y "resource\win\@img" "node_modules\@img"
-    xcopy /E /I /Y "resource\win\sharp" "node_modules\sharp"
-) else (
-    echo No cached folders found, installing sharp...
-    REM 删除现有的文件夹
-    if exist "node_modules\@img" rmdir /s /q "node_modules\@img"
-    if exist "node_modules\sharp" rmdir /s /q "node_modules\sharp"
-    
-    REM 安装 sharp
-    call npm install sharp
-    
-    REM 确保 resource/win 目录存在
-    if not exist "resource\win" mkdir "resource\win"
-    
-    REM 复制到 resource/win 作为缓存
-    xcopy /E /I /Y "node_modules\@img" "resource\win\@img"
-    xcopy /E /I /Y "node_modules\sharp" "resource\win\sharp"
-)
 xcopy /E /I /Y "static\html" "..\resource\"
 call webpack --config webpack.config.js --mode development
 call electron .
