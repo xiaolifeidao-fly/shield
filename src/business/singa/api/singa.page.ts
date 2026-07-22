d24a55aae43b822191c2810517413918a9bae187
tanhai <tanhai@wacai.com>
2026-07-22 10:37:53 +0800

    超时时间 1 分钟

--- src/business/singa/api/singa.page.ts
+++ src/business/singa/api/singa.page.ts
@@ -1,7 +1,7 @@
 import { getPage } from '@src/business/common/engine.manager';
 import { Page } from 'playwright-core';
 
-export const SINGA_REQUEST_TIMEOUT_MS = 3 * 60 * 1000;
+export const SINGA_REQUEST_TIMEOUT_MS = 60 * 1000;
 
 export async function getSingaPage(resourceId: string, url: string): Promise<Page | undefined> {
   return await getPage(resourceId, url, SINGA_REQUEST_TIMEOUT_MS) as unknown as Page | undefined;
